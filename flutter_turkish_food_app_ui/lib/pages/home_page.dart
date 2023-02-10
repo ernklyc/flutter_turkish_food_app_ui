649401d19b4c4e44157cee2060860b54308bfde3
KLYCHub <klychub@gmail.com>
2023-02-11 02:16:40 +0300

    update

---
+++ flutter_turkish_food_app_ui/lib/pages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';zz
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 7,
+            child: Container(color: Colors.amber),
+          ),
+          Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: Text(
+                    "The Fastest in Delivery Food",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          color: const Color.fromARGB(255, 244, 244, 244),
+                          fontWeight: FontWeight.w700,
+                        ),
+                  ),
+                ),
+                SizedBox(
+                  width: 270,
+                  child: Text(
+                    "Our job is to filling your tummy with delicious food and fast delivery",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: const Color.fromARGB(255, 184, 184, 184),
+                          fontWeight: FontWeight.w500,
+                        ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Get Started",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
